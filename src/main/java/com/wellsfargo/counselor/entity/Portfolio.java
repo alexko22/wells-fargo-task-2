443f2d6ce8c9c510f9a065c932871b6969a4ead5
Alex Olson <alexko22222@gmail.com>
2026-02-23 13:10:27 -0500

    implemented final models

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,37 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Client client;
+
+    protected Portfolio() {
+    }
+
+    public Portfolio(Client client) {
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
